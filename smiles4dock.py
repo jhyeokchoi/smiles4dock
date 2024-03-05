1efe67a9c3c97a144813cd07cdfc2fc304f3fc2a
jhyeokchoi <74643891+jhyeokchoi@users.noreply.github.com>
2024-03-05 16:23:09 +0900

    Update smiles4dock.py
    Change version ETKDGv3

--- smiles4dock.py
+++ smiles4dock.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 ## Made by Joonhyeok Choi
 ## For converting ZINC SMILES to MOL, SDF, PDB, or PDBQT format
+## Using ETKDGv3 version
+
 
 import os
 import csv
@@ -9,59 +11,45 @@ from rdkit.Chem import AllChem
 import argparse
 from multiprocessing import Pool, cpu_count
 from oddt.toolkits.extras.rdkit import MolToPDBQTBlock
+from random import *
 
-def smiles_to_mol(args):
-    smiles, output_dir, zinc_id = args
+def smiles_to_format(args):
+    smiles, output_dir, zinc_id, output_format = args
     print(f"Processing {zinc_id}")
+    params = AllChem.ETKDGv3()
+    params.useSmallRingTorsions = True
+    params.useRandomCoords = True
+    params.useMacrocyclesTorsions = True
+    params.useMachineLearning = True
+    params.numThreads = 4
+    params.randomSeed = randrange(10000)
+
     mol = Chem.MolFromSmiles(smiles)
-    mol_h = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol_h, AllChem.ETKDG())
-    #AllChem.MMFFOptimizeMolecule(mol_h)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMultipleConfs(mol, 10, params)
+    #AllChem.EmbedMolecule(mol, params)
+    #AllChem.UFFOptimizeMolecule(mol)
 
-    # Save the molecule in mol format
-    output_file = os.path.join(output_dir, f"{zinc_id}.mol")
-    Chem.MolToMolFile(mol_h, output_file)
-
-def smiles_to_sdf(args):
-    smiles, output_dir, zinc_id = args
-    print(f"Processing {zinc_id}")
-    mol = Chem.MolFromSmiles(smiles)
-    mol_h = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol_h, AllChem.ETKDG())
-    #AllChem.MMFFOptimizeMolecule(mol_h)
-
-    # Save the molecule in sdf format
-    output_file = os.path.join(output_dir, f"{zinc_id}.sdf")
-    writer = Chem.SDWriter(output_file)
-    writer.write(mol_h)
-    writer.close()
-
-def smiles_to_pdb(args):
-    smiles, output_dir, zinc_id = args
-    print(f"Processing {zinc_id}")
-    mol = Chem.MolFromSmiles(smiles)
-    mol_h = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol_h, AllChem.ETKDG())
-    #AllChem.MMFFOptimizeMolecule(mol_h)
-
-    # Save the molecule in pdb format
-    output_file = os.path.join(output_dir, f"{zinc_id}.pdb")
-    Chem.MolToPDBFile(mol_h, output_file)
-
-def smiles_to_pdbqt(args):
-    smiles, output_dir, zinc_id = args
-    print(f"Processing {zinc_id}")
-    mol = Chem.MolFromSmiles(smiles)
-    mol_h = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol_h, AllChem.ETKDG())
-    #AllChem.MMFFOptimizeMolecule(mol_h)
-
-    # Save the molecule in pdbqt format
-    output_file = os.path.join(output_dir, f"{zinc_id}.pdbqt")
-    with open(output_file, 'w') as f:
-        f.write(f"REMARK  Converted from SMILES to PDBQT format\n")
-        f.write(MolToPDBQTBlock(mol_h, computeCharges=True))
-
+    # Save the molecule in format
+    if output_format == 'mol':
+        output_file = os.path.join(output_dir, f"{zinc_id}.mol")
+        Chem.MolToMolFile(mol, output_file)
+    elif output_format == 'sdf':
+        output_file = os.path.join(output_dir, f"{zinc_id}.sdf")
+        writer = Chem.SDWriter(output_file)
+        writer.write(mol)
+        writer.close()
+    elif output_format == 'pdb':
+        output_file = os.path.join(output_dir, f"{zinc_id}.pdb")
+        Chem.MolToPDBFile(mol, output_file)
+    elif output_format == 'pdbqt':
+        output_file = os.path.join(output_dir, f"{zinc_id}.pdbqt")
+        with open(output_file, 'w') as f:
+            f.write(f"REMARK  Converted from SMILES to PDBQT format\n")
+            f.write(MolToPDBQTBlock(mol, computeCharges=True))
+    else:
+        print("Invalid output format. Please choose 'mol', 'sdf', 'pdb', or 'pdbqt'.")
+        
 def process_data_file(data_file, output_dir, output_format):
     zinc_dict = {}
 
@@ -73,18 +61,10 @@ def process_data_file(data_file, output_dir, output_format):
             zinc_dict[zinc_id] = smiles
 
     with Pool(cpu_count()) as pool:
-        args_list = [(smiles, output_dir, zinc_id) for zinc_id, smiles in zinc_dict.items()]
+        args_list = [(smiles, output_dir, zinc_id, output_format) for zinc_id, smiles in zinc_dict.items()]
 
-        if output_format == 'mol':
-            pool.map(smiles_to_mol, args_list)
-        elif output_format == 'sdf':
-            pool.map(smiles_to_sdf, args_list)
-        elif output_format == 'pdb':
-            pool.map(smiles_to_pdb, args_list)
-        elif output_format == 'pdbqt':
-            pool.map(smiles_to_pdbqt, args_list)
-        else:
-            print("Invalid output format. Please choose 'mol', 'sdf', 'pdb', or 'pdbqt'.")
+        pool.map(smiles_to_format, args_list)
+            
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Convert ZINC SMILES to MOL, SDF, PDB, or PDBQT format.")
@@ -103,6 +83,7 @@ if __name__ == "__main__":
         os.makedirs(output_dir)
     else: #os.path.exists(output_dir)
         print(f"Output directory '{output_dir}' already exists. Please choose a different directory.")
+        exit(0)
     
     
     process_data_file(data_file, output_dir, output_format)
